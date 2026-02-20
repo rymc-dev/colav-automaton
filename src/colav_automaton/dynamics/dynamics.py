6c3141e56211f97bfa0218667df4f87853a82466
Ryan McKee <r.mckee@qub.ac.uk>
2026-02-20 19:20:46 +0000

    dynamics refactor

--- src/colav_automaton/dynamics/dynamics.py
+++ src/colav_automaton/dynamics/dynamics.py
@@ -30,48 +30,6 @@ def constant_heading_dynamics(
 
     return np.array([px_dot, py_dot, theta_dot, v_dot, yaw_rate_dot], dtype=float)
 
-# def constant_heading_dynamics(
-#     x: Automaton.Runtime.ContinousState,
-#     aux_x: Dict[str, Automaton.Runtime.AuxiliaryState],
-#     u: Dict[str, Automaton.Runtime.ControlInput],
-#     cfg: Dict,
-#     clk: Automaton.Runtime.Clock
-# ):
-#     """
-#     State x: [px, py, theta, v, yaw_rate]
-#     Slowly converges heading toward the waypoint during cruise
-#     """
-#     px, py, theta, v, yaw_rate = x.get_continous_state()
-    
-#     v_des = cfg.get("constant_velocity", 2.0)  # target speed
-#     k_v   = cfg.get("k_v", 1.0)                # velocity gain
-    
-#     # Get waypoint for heading reference
-#     x_wp, y_wp = aux_x["waypoints"].state[0]
-    
-#     # Compute desired heading toward waypoint
-#     desired_heading = math.atan2(y_wp - py, x_wp - px)
-    
-#     # Heading error in [-pi, pi]
-#     e_theta = (desired_heading - theta + math.pi) % (2 * math.pi) - math.pi
-    
-#     # Very slow heading convergence gain (much smaller than LOS)
-#     k_theta_cruise = cfg.get("k_theta_cruise", 0.1)  # slow convergence
-    
-#     # kinematics
-#     px_dot = v * math.cos(theta)
-#     py_dot = v * math.sin(theta)
-    
-#     # Slow heading convergence
-#     theta_dot = k_theta_cruise * e_theta
-    
-#     # drive v -> v_des
-#     v_dot = k_v * (v_des - v)
-    
-#     yaw_rate_dot = 0.0
-    
-#     return np.array([px_dot, py_dot, theta_dot, v_dot, yaw_rate_dot], dtype=float)
-
 @continuous_dynamics
 def flow_los_heading(
     ctx: RuntimeContext
@@ -112,4 +70,5 @@ def flow_los_heading(
     yaw_rate_dot = 0.0
 
     return np.array([px_dot, py_dot, theta_dot, v_dot, yaw_rate_dot], dtype=float)
+
 
